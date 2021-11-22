1022b0cc8f6d69185bbc11ff147eeb09e861e89d
Antonio Monroy <tony_monroe1212@hotmail.com>
2021-11-22 10:05:53 -0600

    Added Tailwind

--- web-app/entry-manager/src/components/DeviceCard.jsx
+++ web-app/entry-manager/src/components/DeviceCard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const DeviceCard = () => {
   return (
-    <div className="card" style={{ width: "300px", height: "260px" }}>
+    <div className="card shadow" style={{ width: "300px", height: "260px" }}>
       <div className="card-header">Device From somewhere</div>
       <div className="card-body">Location: somewhere</div>
       <div className="card-footer">
-        <button className="btn btn-info btn-block">Ver dispositivo</button>
+        <Link to="/device/1" className="btn btn-info btn-block">
+          Ver dispositivo
+        </Link>
       </div>
     </div>
   );
